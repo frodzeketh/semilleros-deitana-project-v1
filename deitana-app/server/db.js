1f4a53eebab7aff2164d32e520feb5175e466bde
Rodrigo Facundo <frodzek@proton.me>
2025-05-18 00:03:22 -0300

    Actualizando conexión a la base de datos para usar URL pública

--- deitana-app/server/db.js
+++ deitana-app/server/db.js
@@ -1,25 +1,20 @@
 const mysql = require('mysql2/promise');
 require('dotenv').config();
 
-console.log('Intentando conectar a la base de datos con:', {
-  host: process.env.MYSQLHOST,
-  user: process.env.MYSQLUSER,
-  database: process.env.MYSQLDATABASE,
-  port: process.env.MYSQLPORT
-});
+console.log('Intentando conectar a la base de datos...');
 
 const pool = mysql.createPool({
-  host: process.env.MYSQLHOST,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
-  port: process.env.MYSQLPORT,
+  host: 'centerbeam.proxy.rlwy.net',
+  user: 'root',
+  password: 'gbrIerodvEYzzDQbgtlQjelgLaLlgPuf',
+  database: 'railway',
+  port: 32877,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
 });
 
-// Agregar un test de conexión
+// Test de conexión
 pool.getConnection()
   .then(connection => {
     console.log('Conexión exitosa a la base de datos');
